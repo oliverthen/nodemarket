df10d4d6718fc3def7f3bd688174dcc8e33fdaeb
Oliver Then <oli171dr@gmail.com>
2024-10-08 06:56:10 -0400

    Create, serve html files, and added styling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 
@@ -8,12 +8,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).send('<h1>Page not found</h1>');
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
