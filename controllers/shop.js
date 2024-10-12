676c07b0d6a70d662db6134a85c20cfbe9ae4821
Oliver Then <oli171dr@gmail.com>
2024-10-12 08:59:42 -0400

    Extracting Dynamic Product params and displaying info about that product

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,6 +7,18 @@ exports.getProducts = (req, res, next) => {
 	});
 };
 
+exports.getProduct = (req, res, next) => {
+	const prodId = req.params.productId;
+	Product.findById(prodId, product => {
+		res.render('shop/product-detail', {
+			product,
+			pageTitle: product.title,
+			path: '/products'
+		});
+	})
+	
+};
+
 exports.getIndex = (req, res, next) => {
 	Product.fetchAll((products) => {
 		res.render('shop/index', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true});
