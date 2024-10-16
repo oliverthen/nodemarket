3940b75e9a43cb4a36b84a23af61e0de485c376b
Oliver Then <oli171dr@gmail.com>
2024-10-16 10:22:28 -0400

    Delete product capability added and now able to display cart items and quantity

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 
 exports.getAddProduct = (req, res, next) => {
-	res.render('admin/add-product', {pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true});
+	res.render('admin/edit-product', {pageTitle: 'Add Product', path: '/admin/add-product', editing: false});
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -10,9 +10,40 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const description = req.body.description;
 	const price = req.body.price;
-	const product = new Product(title, imageUrl, description, price);
+	const product = new Product(null, title, imageUrl, description, price);
 	product.save();
 	res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+	const editMode = req.query.edit;
+	if (!editMode) {
+		return res.redirect('/');
+	}
+	const prodId = req.params.productId;
+	Product.findById(prodId, product => {
+		if (!product) {
+			return res.redirect('/');
+		}
+		res.render('admin/edit-product', {
+		pageTitle: 'Edit Product', 
+		path: '/admin/edit-product',
+		editing: editMode,
+		product
+	});
+	});
+	
+};
+
+exports.postEditProduct = (req, res, next) => {
+	const prodId = req.body.productId;
+	const updatedTitle = req.body.title;
+	const updatedPrice = req.body.price;
+	const updatedImageUrl = req.body.imageUrl;
+	const updatedDescription = req.body.description;
+	const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDescription, updatedPrice);
+	updatedProduct.save();
+	res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
@@ -20,3 +51,9 @@ exports.getProducts = (req, res, next) => {
 		res.render('admin/products', {prods: products, pageTitle: 'Admin Products', path: '/admin/products', hasProducts: products.length > 0, activeShop: true, productCSS: true});
 	});
 }
+
+exports.postDeleteProduct = (req, res, next) => {
+	const prodId = req.body.productId;
+	Product.deleteById(prodId);
+	res.redirect('/admin/products');
+};
