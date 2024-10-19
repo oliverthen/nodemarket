f3c819154c2b81e0cb6cb8deaa8fea05a89bc507
Oliver Then <oli171dr@gmail.com>
2024-10-19 16:05:17 -0400

    Incoporated use of MySQL for storage and retrieval of products data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
