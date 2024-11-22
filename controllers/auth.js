5d06968b4808d65a8988614bd978293d3697919f
Oliver Then <oli171dr@gmail.com>
2024-11-22 11:34:52 -0500

    Added login functionality and added a middleware function to protect certain routes based upon if a user is logged in or not

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,15 +20,28 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-	User.findById('67378d392c6b5991a468c105')
+	const email = req.body.email;
+	const password = req.body.password
+	User.findOne({email})
 		.then(user => {
-			req.session.isLoggedIn = true;
-			req.session.user = user;
-			req.session.save((err) => {
-				console.log(err);
-				res.redirect('/');
-			})
-  		
+			if (!user) {
+				return res.redirect('/login');
+			}
+			bcrypt.compare(password, user.password)
+				.then(doMatch => {
+					if (doMatch) {
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save((err) => {
+							console.log(err);
+							res.redirect('/');
+						})					}
+					res.redirect('/login');
+				})
+				.catch(err => {
+					console.log(err);
+					res.redirect('/login');
+				});  		
 		})
 		.catch(err => {
 			console.log(err);
