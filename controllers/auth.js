559aadb18e76eec6612099f3e8f3912d1e5cb4fd
Oliver Then <oli171dr@gmail.com>
2024-11-28 14:53:45 -0500

    Use Express Validator to display error message upon invalid email input

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 
 exports.getLogin = (req, res, next) => {
 	let message = req.flash('error');
@@ -69,6 +69,15 @@ exports.postSignup = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
 	const confirmPassword = req.body.confirmPassword;
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		console.log(errors.array());
+		return res.status(422).render('auth/signup', {
+			path: '/signup',
+			pageTitle: 'Signup',
+			errorMessage: errors.array()[0].msg
+		});
+	}
 	User.findOne({email})
 		.then(userDoc => {
 			if (userDoc) {
