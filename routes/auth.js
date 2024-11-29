ff26e23f9702a7af3975aee38389a339a02979e6
Oliver Then <oli171dr@gmail.com>
2024-11-29 09:58:56 -0500

    Added password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -11,12 +11,28 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.').custom((value, {req}) => {
-	if (value === 'test@test.com') {
-		throw new Error('This email address is forbidden.');
-	}
-	return true;
-}), authController.postSignup);
+router.post('/signup',
+	[
+	check('email')
+		.isEmail()
+		.withMessage('Please enter a valid email.')
+		.custom((value, {req}) => {
+			if (value === 'test@test.com') {
+				throw new Error('This email address is forbidden.');
+			}
+			return true;
+		}),
+	body('password', 'Please enter a password with only numbers and text and at least 5 characters')
+		.isLength({ min: 5 })
+		.isAlphanumeric(),
+	body('confirmPassword')
+		.custom((value, {req}) => {
+			if (value !== req.body.password) {
+				throw new Error ('Passwords have to match!')
+			}
+			return true;
+		})
+	], authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
