2a99563ab68b5bf0f60f82e20429d74a8694474a
Oliver Then <oli171dr@gmail.com>
2025-03-01 17:49:51 -0500

    Now returning error page when adding product

--- app.js
+++ app.js
@@ -60,6 +60,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500)
+
 app.use(errorController.get404);
 mongooseConnect()
 	.then(result => {		
